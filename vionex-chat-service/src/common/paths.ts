8ef61233dbb812481b1ea1545a115fb2565b1ce3
xuantruongg003 <lexuantruong098@gmail.com>
2025-10-23 21:53:09 +0700

    fix proto path

--- vionex-chat-service/src/common/paths.ts
+++ vionex-chat-service/src/common/paths.ts
@@ -2,4 +2,5 @@ import { resolve } from 'path';
 
 export const protoPaths = {
   semantic: resolve(__dirname, '../../../protos/semantic.proto'),
+  chat: resolve(__dirname, '../../../protos/chat.proto'),
 };
